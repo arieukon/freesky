30cdf9a1c8c6d3fc2c77624b3b9202001eaf2823
Anshari Furqan <anshari.furqan@yahoo.com>
2016-06-21 15:27:02 +0700

    Video page

--- persentation/src/main/java/com/kulomady/freesky/view/activity/video/VideoActivity.java
+++ persentation/src/main/java/com/kulomady/freesky/view/activity/video/VideoActivity.java
@@ -1,31 +1,63 @@
 package com.kulomady.freesky.view.activity.video;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.kulomady.freesky.R;
+import com.kulomady.freesky.view.activity.home.HomeActivity;
 
-public class VideoActivity extends AppCompatActivity {
+import butterknife.BindView;
+
+public class VideoActivity extends AppCompatActivity implements com.kulomady.freesky.view.activity.video.VideoView,
+        View.OnClickListener {
+
+/*
+    @BindView(R.id.img_video)
+    ImageView mImgVideo;
+*/
+    ImageView mImgVideo;
+
+    @BindView(R.id.btn_skip)
+    Button mBtnSkip;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle  savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
+    @Override
+    public void initView() {
+        mImgVideo = (ImageView) findViewById(R.id.img_video);
+
+        mImgVideo.setOnClickListener(this);
+        mBtnSkip.setOnClickListener(this);
+        mBtnSkip.setOnClickListener(this);
+
+        gotoHome();
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if(id==mImgVideo.getId()){
+            gotoHome();
+        } else if(id==mBtnSkip.getId()){
+            gotoHome();
+        }
+    }
+
+    @Override
+    public void gotoHome() {
+        Toast.makeText(this, "dsfdsfdf", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(VideoActivity.this, HomeActivity.class);
+        startActivity(intent);
+        finish();
+    }
 }
---
+++ persentation/src/main/java/com/kulomady/freesky/view/activity/video/VideoView.java
@@ -0,0 +1,9 @@
+package com.kulomady.freesky.view.activity.video;
+
+/**
+ * Created by macaris on 6/21/16.
+ */
+public interface VideoView {
+    void initView();
+    void gotoHome();
+}
